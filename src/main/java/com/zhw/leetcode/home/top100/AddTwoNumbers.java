0e963f9fd5e54b0b919313142680bccb2bad9dae
BruceZhang54110 <zhw54110@163.com>
2021-06-29 00:28:44 +0800

    两数之和的第二种写法

--- src/main/java/com/zhw/leetcode/home/top100/AddTwoNumbers.java
+++ src/main/java/com/zhw/leetcode/home/top100/AddTwoNumbers.java
@@ -40,48 +40,35 @@ public class AddTwoNumbers {
     public static void main(String[] args) {
 
     }
-
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = null;
-        ListNode tail = null;
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
         // 进位
         int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int n1 = l1 == null ? 0 : l1.val;
+            int n2 = l2 == null ? 0 : l2.val;
 
-        while (l1 != null || l2 != null) {
-            int n1 = 0;
-            int n2 = 0;
-            if (l1 != null) {
-                n1 = l1.val;
-            }
-            if (l2 != null) {
-                n2 = l2.val;
-            }
             int sum = n1 + n2 + carry;
-            if (head == null) {
-                head = tail = new ListNode(sum % 10);
-            } else {
-                tail.next = new ListNode(sum % 10);
-                tail = tail.next;
-            }
-            carry = sum / 10;
+            head.next = new ListNode(sum % 10);
+            head = head.next; // head 前进一位
+            carry = sum / 10; // 计算进位,取整除的整数
             if (l1 != null) {
                 l1 = l1.next;
             }
             if (l2 != null) {
                 l2 = l2.next;
             }
+
         }
-        if (carry > 0) {
-            tail.next = new ListNode(carry);
-        }
-        return head;
+
+        return dummy.next;
     }
 
     public static class ListNode {
         int val;
         ListNode next;
         ListNode() {
-
         }
         ListNode(int val) {
             this.val = val;
