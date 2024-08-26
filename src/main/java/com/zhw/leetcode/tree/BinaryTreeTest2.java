95cf1145a83130f0b0c165c2c9994e3401ae148a
BruceZhang54110 <zhw54110@163.com>
2024-08-26 23:53:51 +0800

    广度优先遍历

--- src/main/java/com/zhw/leetcode/tree/BinaryTreeTest2.java
+++ src/main/java/com/zhw/leetcode/tree/BinaryTreeTest2.java
@@ -1,11 +1,14 @@
 package com.zhw.leetcode.tree;
 
+import com.sun.source.tree.Tree;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
  * 二叉树层序遍历-广度优先搜索
+ * 知识点：二叉树，队列
  */
 public class BinaryTreeTest2 {
     public static void main(String[] args) {
@@ -25,10 +28,35 @@ public class BinaryTreeTest2 {
         root.setLeftNode(leftNode);
         TreeNode rightNode = new TreeNode(2);
         root.setRightNode(rightNode);
-        List<List<Integer>> lists = levelOrder(root);
+        // List<List<Integer>> lists = levelOrder(root);
+        List<List<Integer>> lists = levelOrderRecursion(root);
         System.out.println(lists);
 
 
+    }
+
+    /**
+     * 递归解法
+     * @return
+     */
+    public static List<List<Integer>> levelOrderRecursion(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        addNode(root, 0, result);
+        return result;
+
+    }
+
+    private static void addNode(TreeNode node, int level, List<List<Integer>> levels) {
+        if (levels.size() == level) {
+            levels.add(new ArrayList<>());
+        }
+        levels.get(level).add(node.val);
+        if (node.leftNode != null) {
+            addNode(node.leftNode, level + 1, levels);
+        }
+        if (node.rightNode != null) {
+            addNode(node.rightNode, level + 1, levels);
+        }
     }
 
     public static List<List<Integer>> levelOrder(TreeNode root) {
